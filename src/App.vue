9764f58b89ecf27eac7b8cf6caca6c278f7e558e
VanHungNguyen <33939159+VanHungNguyenn@users.noreply.github.com>
2023-12-04 23:29:23 +0700

    initial vue-i-money

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { BeakerIcon, AcademicCapIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+	<h1 class="text-3xl font-bold underline">Hello world!</h1>
+	<BeakerIcon class="h-6 w-6" />
+	<AcademicCapIcon class="h-6 w-6" />
+</template>
+
+<style scoped></style>
