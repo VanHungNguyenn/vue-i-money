4824950b79604b1146df133397e3f592be74a324
VanHungNguyen <33939159+VanHungNguyenn@users.noreply.github.com>
2023-12-06 23:33:52 +0700

    .

--- src/composables/useSignUp.js
+++ src/composables/useSignUp.js
@@ -5,7 +5,34 @@ const error = ref(null)
 const isPending = ref(false)
 
 async function signup(email, password, fullName) {
-	await auth.createUserWithEmailAndPassword(email, password)
+	error.value = null
+	isPending.value = true
+
+	try {
+		// Create user with email and password
+		const response = await auth.createUserWithEmailAndPassword(
+			email,
+			password
+		)
+
+		if (!response) {
+			throw new Error('Failed to create new user')
+		}
+
+		// Update profile with full name
+		await response.user.updateProfile({ displayName: fullName })
+
+		// Reset states after successful signup
+		isPending.value = false
+
+		return response
+	} catch (err) {
+		console.error(err.message)
+		error.value = err.message
+		isPending.value = false
+	}
 }
 
-export function useSignUp() {}
+export function useSignUp() {
+	return { error, isPending, signup }
+}
